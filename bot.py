8177a311e75cd0c41fdb2442a9daf5c73d68f71b
abuztrade <abuztrade.work@gmail.com>
2025-03-12 01:52:27 +0300

    update version

--- bot.py
+++ bot.py
@@ -2,18 +2,17 @@
 
 import asyncio
 import re
-from sys import stderr
 import time
 
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, User as TelegramUser
 from aiogram.filters.command import Command
+from aiogram.types import Message
+from aiogram.types import User as TelegramUser
 from dotenv import dotenv_values
-from loguru import logger
-from peewee import BooleanField, CharField, BigIntegerField, Model
+from peewee import BigIntegerField, BooleanField, CharField, Model
 from playhouse.postgres_ext import PostgresqlExtDatabase
 
-from refer import async_login_with_invite_code
+from refer import async_login_with_invite_code, logger
 
 config = dotenv_values(".env")
 DB_NAME = config.get("DB_NAME", "postgres")
@@ -25,6 +24,7 @@ DB_PORT = int(config.get("DB_PORT", "5432"))
 db = PostgresqlExtDatabase(
     database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
 )
+
 
 class User(Model):
     user_id = BigIntegerField(unique=True)
@@ -40,18 +40,6 @@ class User(Model):
 
 User.create_table(safe=True)
 
-logger.remove()
-logger.add(stderr)
-
-logger.add(
-    "bot.log",
-    backtrace=True,
-    diagnose=True,
-)
-
-logger.level("DEBUG", color="<magenta>")
-
-
 PROXY = config.get("PROXY", None)
 
 dp = Dispatcher()
@@ -59,19 +47,21 @@ dp = Dispatcher()
 users_last_coffee = {}
 users_last_message = {}
 
+
 def check_user(tg_user: TelegramUser) -> User:
-    user: User = User.get_or_none(user_id = tg_user.id)
+    user: User = User.get_or_none(user_id=tg_user.id)
     if not user:
         user = User(
-            user_id = tg_user.id,
-            username = tg_user.username,
-            first_name = tg_user.first_name,
-            last_name = tg_user.last_name,
-            is_admin = False
+            user_id=tg_user.id,
+            username=tg_user.username,
+            first_name=tg_user.first_name,
+            last_name=tg_user.last_name,
+            is_admin=False,
         )
         user.save()
 
     return user
+
 
 @dp.message(F.chat.type == "private", Command("coffee"))
 async def coffee(message: Message):
@@ -101,7 +91,9 @@ async def coffee(message: Message):
             await message.answer(usage)
             return
 
-        await message.answer(f"<b>💋 Идёт отправка {count} кофе...</b>\n\n<b>💋 Sending {count} coffee...</b>")
+        await message.answer(
+            f"<b>💋 Идёт отправка {count} кофе...</b>\n\n<b>💋 Sending {count} coffee...</b>"
+        )
 
         for i in range(count):
             try:
@@ -112,15 +104,19 @@ async def coffee(message: Message):
                     f"[/coffee] Status Code: {status_code} | Response Body: {str(response_body)[:12]}"
                 )
             except Exception:
-                pass 
-        
-        await message.answer(f"<b>❤️ Успешно отправил вам {count} кофе</b>\n\n<b>❤️ Successfully sent you {count} coffee</b>")
+                pass
+
+        await message.answer(
+            f"<b>❤️ Успешно отправил вам {count} кофе</b>\n\n<b>❤️ Successfully sent you {count} coffee</b>"
+        )
 
     except Exception as e:
         logger.error(f"Error: {e}")
         await message.answer(usage)
 
-COFFEE_WAIT = 1 * 60 # 5 min
+
+COFFEE_WAIT = 1 * 60  # 5 min
+
 
 @dp.message(F.chat.type == "private")
 async def start(message: Message):
@@ -131,21 +127,23 @@ async def start(message: Message):
 
         user: User = check_user(message.from_user)
 
-        await message.answer(
-            "<b>💔 Работа бота временно преостановлена из-за блокировок</b>\n\n"
-            "<b>❤️ The bot's work is temporarily suspended due to blockages</b>\n\n"
-
-            "🐙 <b>Source:</b> https://github.com/makarworld/StatusAIFreeCoffee\n"
-            "❤️‍🔥 <b>Channel:</b> @StatusAIFree\n"
-            "🧩 <b>Creator:</b> @abuztrade"
-        )
-        return
+        # await message.answer(
+        #    "<b>💔 Работа бота временно преостановлена из-за блокировок</b>\n\n"
+        #    "<b>❤️ The bot's work is temporarily suspended due to blockages</b>\n\n"
+        #    "🐙 <b>Source:</b> https://github.com/makarworld/StatusAIFreeCoffee\n"
+        #    "❤️‍🔥 <b>Channel:</b> @StatusAIFree\n"
+        #    "🧩 <b>Creator:</b> @abuztrade"
+        # )
+        # return
 
         refcode = re.findall(r"[0-9a-zA-Z]{10}", message.text or "")
 
         if refcode:
             # if last coffee was more than 5 min ago, add
-            if time.time() - users_last_coffee.get(user.user_id, 0) >= COFFEE_WAIT or user.is_admin:
+            if (
+                time.time() - users_last_coffee.get(user.user_id, 0) >= COFFEE_WAIT
+                or user.is_admin
+            ):
                 refcode = refcode[0]
                 user.last_refcode = refcode
                 user.save()
@@ -161,37 +159,41 @@ async def start(message: Message):
                         )
                         break
                     except Exception as e:
-                        logger.error(f"[{r}] Request Error: {e}")
+                       logger.error(f"[{r}] Request Error: {e}")
 
                 await message.answer(
                     "<b>❤️ Успешно отправил вам кофе, следующий кофе через 1 минуту</b>\n\n"
                     "<b>❤️ Successfully sent you coffee, next coffee in 1 minute</b>\n\n"
-
                     "🐙 <b>Source:</b> https://github.com/makarworld/StatusAIFreeCoffee\n"
                     "❤️‍🔥 <b>Channel:</b> @StatusAIFree\n"
                     "🧩 <b>Creator:</b> @abuztrade"
                 )
 
             else:
-                if time.time() - users_last_message.get(user.user_id, 0) >= 5 or user.is_admin:
+                if (
+                    time.time() - users_last_message.get(user.user_id, 0) >= 5
+                    or user.is_admin
+                ):
                     wait_sec = COFFEE_WAIT - (time.time() - users_last_coffee[user.user_id])
                     await message.answer(
                         f"<b>💋 {wait_sec:.0f} сек до следующего кофе</b> <i>(ожидание введено для того чтобы меньше нагружать сервера игры)</i>\n\n"
                         f"<b>💋 {wait_sec:.0f} seconds until the next coffee</b> <i>(waiting is done to not overload the game server)</i>"
                     )
                     users_last_message[user.user_id] = time.time()
-            
+
         else:
             await message.answer(
-                "<b>😈 Отправь мне свой инвайт-код:</b>\n\n<b>😈 Send me your invite code:</b>"
+                "<b>🤰 Отправь мне свой инвайт-код:</b>\n\n"
+                "<b>🤰 Send me your invite code:</b>"
             )
 
     except Exception as e:
-        logger.error(f"Error: {e}")
+       logger.error(f"Error: {e}")
+
 
 @dp.message(F.chat.type == "private", Command("mailing"))
 async def mailing(message: Message):
-    user: User = User.get_or_none(user_id = message.from_user.id)
+    user: User = User.get_or_none(user_id=message.from_user.id)
     if not user or not user.is_admin:
         return
 
@@ -209,9 +211,9 @@ async def mailing(message: Message):
             logger.info(f"Sent mailing to {user.user_id}")
         except Exception as e:
             logger.exception(f"Error: {e}")
-        
+
         await asyncio.sleep(0.5)
-    
+
     await message.answer("<b>✅ Отправлено</b>")
 
 
--- refer.py
+++ refer.py
@@ -1,13 +1,83 @@
 import random
+import re
+import time
 from hashlib import md5
+from sys import stderr
 
 from curl_cffi import requests
+from loguru import logger
+
+logger.remove()
+logger.add(stderr)
+
+logger.add(
+    "bot.log",
+    backtrace=True,
+    diagnose=True,
+)
+
+logger.level("DEBUG", color="<magenta>")
+
+
+apk_cache: list = None
+
+
+async def get_apk_version() -> tuple[str, str]:
+    global apk_cache
+
+    build_pattern = r">\((\d{2})\)<\/span>"
+    version_pattern = r"status - sims but social media (\d+\.\d+\.\d+)"
+
+    if apk_cache is not None:
+        if time.time() - apk_cache[2] < 10 * 60:  # 10 min
+            return apk_cache[:2]
+
+    async with requests.AsyncSession() as session:
+        response = await session.get(
+            "https://apkcombo.app/downloader/?package=link.socialai.app&ajax=1"
+        )
+
+        build = re.search(build_pattern, response.text).group(1)
+        version = re.search(version_pattern, response.text).group(1)
+
+        apk_cache = [build, version, time.time()]
+
+        logger.success(
+            f"Updated APK cache from apkcombo | Build: {build} | Version: {version}"
+        )
+        return apk_cache[:2]
+
+
+def sync_get_apk_version() -> tuple[str, str]:
+    global apk_cache
+
+    build_pattern = r">\((\d{2})\)<\/span>"
+    version_pattern = r"status - sims but social media (\d+\.\d+\.\d+)"
+
+    if apk_cache is not None:
+        if time.time() - apk_cache[2] < 10 * 60:  # 10 min
+            return apk_cache[:2]
+
+    response = requests.get(
+        "https://apkcombo.app/downloader/?package=link.socialai.app&ajax=1"
+    )
+
+    build = re.search(build_pattern, response.text).group(1)
+    version = re.search(version_pattern, response.text).group(1)
+
+    apk_cache = [build, version, time.time()]
+
+    logger.success(
+        f"Updated APK cache from apkcombo | Build: {build} | Version: {version}"
+    )
+    return apk_cache[:2]
 
 
 def login_with_invite_code(invite_code: str):
     random_user_id = random.randint(100000000000000000000, 100999999999999999999)
     device_id = f"md5/{md5(str(random_user_id).encode()).hexdigest()}"
 
+    build, version = sync_get_apk_version()
     # Определяем URL
     url = "https://social-ai-prod.uc.r.appspot.com/user/auth/login"
     # Определяем заголовки
@@ -15,12 +85,12 @@ def login_with_invite_code(invite_code: str):
         "accept": "application/json, text/plain, */*",
         "Accept-Encoding": "gzip",
         "app": "SOCIALAI",
-        "build": "28",
+        "build": build,
         "Connection": "Keep-Alive",
         "device-id": device_id,
         "Host": "social-ai-prod.uc.r.appspot.com",
         "User-Agent": "okhttp/4.12.0",
-        "version": "1.3.1",
+        "version": version,
     }
 
     # Определяем данные формы
@@ -41,6 +111,8 @@ async def async_login_with_invite_code(invite_code: str, proxy: dict = None):
     random_user_id = random.randint(100000000000000000000, 100999999999999999999)
     device_id = f"md5/{md5(str(random_user_id).encode()).hexdigest()}"
 
+    build, version = await get_apk_version()
+
     # Определяем URL
     url = "https://social-ai-prod.uc.r.appspot.com/user/auth/login"
     # Определяем заголовки
@@ -48,12 +120,12 @@ async def async_login_with_invite_code(invite_code: str, proxy: dict = None):
         "accept": "application/json, text/plain, */*",
         "Accept-Encoding": "gzip",
         "app": "SOCIALAI",
-        "build": "29",
+        "build": build,
         "Connection": "Keep-Alive",
         "device-id": device_id,
         "Host": "social-ai-prod.uc.r.appspot.com",
         "User-Agent": "okhttp/4.12.0",
-        "version": "1.3.2",
+        "version": version,
     }
 
     # Определяем данные формы
@@ -64,9 +136,9 @@ async def async_login_with_invite_code(invite_code: str, proxy: dict = None):
     }
 
     # Отправляем PUT-запрос
-    async with requests.AsyncSession(proxy = proxy) as session:
+    async with requests.AsyncSession(proxy=proxy) as session:
         response = await session.put(url, headers=headers, data=data)
-
+    print(response.text)
     # Возвращаем статус-код и тело ответа
     return response.status_code, response.json()
 
