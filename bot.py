06e6e11478ff6890cd989730ccd7f21a41d84bc2
abuztrade <abuztrade.work@gmail.com>
2025-03-11 18:44:57 +0300

    add save users to db

--- bot.py
+++ bot.py
@@ -3,12 +3,39 @@
 import re
 from sys import stderr
 
-from aiogram import F, Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.types import Message
 from dotenv import dotenv_values
 from loguru import logger
+from peewee import BooleanField, CharField, IntegerField, Model
+from playhouse.postgres_ext import PostgresqlExtDatabase
 
 from refer import async_login_with_invite_code
+
+config = dotenv_values(".env")
+DB_NAME = config.get("DB_NAME", "postgres")
+DB_USER = config.get("DB_USER", "postgres")
+DB_PASS = config.get("DB_PASS", "postgres")
+DB_HOST = config.get("DB_HOST", "postgres")
+DB_PORT = int(config.get("DB_PORT", "5432"))
+
+db = PostgresqlExtDatabase(
+    database=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT
+)
+
+class User(Model):
+    user_id = IntegerField(unique=True)
+    username = CharField(null=True)
+    first_name = CharField(null=True)
+    last_name = CharField(null=True)
+    last_refcode = CharField(null=True)
+    is_admin: bool = BooleanField(default=False)
+
+    class Meta:
+        database = db
+
+
+User.create_table(safe=True)
 
 logger.remove()
 logger.add(stderr)
@@ -22,8 +49,6 @@ logger.add(
 logger.level("DEBUG", color="<magenta>")
 
 
-config = dotenv_values(".env")
-
 PROXY = config.get("PROXY", None)
 
 dp = Dispatcher()
@@ -36,12 +61,28 @@ async def start(message: Message):
             f"User: @{message.from_user.username} |  ID: {message.from_user.id} | Text: {message.text}"
         )
 
+        user, _ = User.get_or_create(user_id = message.from_user.id)
+        user: User 
+
+        user.username = message.from_user.username
+        user.first_name = message.from_user.first_name
+        user.last_name = message.from_user.last_name
+        user.save()
+
+
         refcode = re.findall(r"[0-9a-zA-Z]{10}", message.text or "")
 
         if refcode:
             refcode = refcode[0]
-            status_code, response_body = await async_login_with_invite_code(refcode, proxy = PROXY)
-            logger.info(f"Status Code: {status_code} | Response Body: {str(response_body)[12:]}")
+            user.last_refcode = refcode
+            user.save()
+            
+            status_code, response_body = await async_login_with_invite_code(
+                refcode, proxy=PROXY
+            )
+            logger.info(
+                f"Status Code: {status_code} | Response Body: {str(response_body)[12:]}"
+            )
             await message.answer(
                 "<b>❤️ Успешно отправил вам кофе</b>\n\n<b>❤️ Successfully sent you coffee</b>"
             )
